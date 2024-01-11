72b5909dbfafdf41f88c248d7aacbb31fecf85ba
James Grieve <jamesonrgrieve@gmail.com>
2024-01-11 13:15:05 -0700

    Resolve linting issues;

--- src/js/console.js
+++ src/js/console.js
@@ -1,65 +1,67 @@
-let inputBar = document.querySelector("#input");
-let outputPanel = document.querySelector("#output");
+/* global main */
+const inputBar = document.querySelector("#input");
+const outputPanel = document.querySelector("#output");
 
 let proceed = false;
-const timeout = async ms => new Promise(res => setTimeout(res, ms));
-function output(string, type="normal") {
-    // Anything passed into here will be printed in the web page.
-    let newP = document.createElement("p");
-    let text = String(string).replaceAll(' ', '\xa0');
-    if (type == "debug") {
-        text = "DEBUG: "+text;
-    }
-    newP.innerText = text;
-    if (type != "normal")
-    {
-        newP.classList.add(type);
-    }
-    document.querySelector("#output").appendChild(newP);
+const timeout = async (ms) => new Promise((res) => setTimeout(res, ms));
+function output(string, type = "normal") {
+  // Anything passed into here will be printed in the web page.
+  const newP = document.createElement("p");
+  let text = String(string).replaceAll(" ", "\xa0");
+  if (type === "debug") {
+    text = "DEBUG: " + text;
+  }
+  newP.innerText = text;
+  if (type !== "normal") {
+    newP.classList.add(type);
+  }
+  document.querySelector("#output").appendChild(newP);
 }
+// eslint-disable-next-line no-unused-vars
 async function input(string) {
-    let newP = document.createElement("p");
-    newP.innerHTML = string + '<span class="editing"></span>';
-    document.querySelector("#output").appendChild(newP);
-    inputBar.focus();
-    while (proceed === false) { await timeout(500); newP.classList.length === 0 ? newP.classList.add("flash") : newP.classList.remove("flash"); }
-    let text = inputBar.value;
-    inputBar.value = "";
-    newP.classList.remove("flash");
-    newP.querySelector("span").classList.remove("editing");
-    proceed = false;
-    return text;
+  const newP = document.createElement("p");
+  newP.innerHTML = string + '<span class="editing"></span>';
+  document.querySelector("#output").appendChild(newP);
+  inputBar.focus();
+  while (proceed === false) {
+    await timeout(500);
+    newP.classList.length === 0 ? newP.classList.add("flash") : newP.classList.remove("flash");
+  }
+  const text = inputBar.value;
+  inputBar.value = "";
+  newP.classList.remove("flash");
+  newP.querySelector("span").classList.remove("editing");
+  proceed = false;
+  return text;
 }
 function submit() {
-    proceed = true;
+  proceed = true;
 }
-inputBar.addEventListener("keyup", function(event) {
-    document.querySelector(".editing").innerText = inputBar.value.replaceAll(' ', '\xa0');
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      submit();
-    }
-  });
-outputPanel.addEventListener("click", function(event) {
-    inputBar.focus();
+inputBar.addEventListener("keyup", function (event) {
+  document.querySelector(".editing").innerText = inputBar.value.replaceAll(" ", "\xa0");
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    submit();
+  }
+});
+outputPanel.addEventListener("click", function () {
+  inputBar.focus();
 });
 
-function refreshTheme()
-{
-    let body = document.querySelector("body");   
-    body.classList.remove(...body.classList);
-    body.classList.add(localStorage.getItem('exsm3935-theme'));
+function refreshTheme() {
+  const body = document.querySelector("body");
+  body.classList.remove(...body.classList);
+  body.classList.add(localStorage.getItem("exsm3935-theme"));
 }
-for (item of document.querySelectorAll(".theme"))
-{
-    item.addEventListener("click", function(event) {
-        localStorage.setItem('exsm3935-theme', event.target.classList[0]);
-        refreshTheme();
-    });
+for (const item of document.querySelectorAll(".theme")) {
+  item.addEventListener("click", function (event) {
+    localStorage.setItem("exsm3935-theme", event.target.classList[0]);
+    refreshTheme();
+  });
 }
 async function run() {
-    await main().catch((e) => output("\n---UNHANDLED EXCEPTION---\n"+e.stack, "error"));
-    output("\nEnd of program, please refresh the page (F5) to restart the program.", "meta");
+  await main().catch((e) => output("\n---UNHANDLED EXCEPTION---\n" + e.stack, "error"));
+  output("\nEnd of program, please refresh the page (F5) to restart the program.", "meta");
 }
 refreshTheme();
 run();
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,13 @@
+/* global output, input */
+// eslint-disable-next-line no-unused-vars
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
+  // This is where the code you're actually experimenting with goes.
 
-    while (name != "Exit") 
-    {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
-    }
+  const prompt = "Please enter your name, or 'Exit' to quit: ";
+  let name = await input(prompt);
+
+  while (name !== "Exit") {
+    output("Hello, " + name + "!");
+    name = await input(prompt);
+  }
 }
-
-
